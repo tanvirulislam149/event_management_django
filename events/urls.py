8d64db48df0301f5f696d257992440d7b995b30e
tanvir <tanvirulislam149@gmail.com>
2025-01-28 16:36:48 +0600

    all update done

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from events.views import dashboard, create_event
+from events.views import dashboard, create_event, update_event
 
 urlpatterns = [
     path("", dashboard),
-    path("create_event/<int:id>/", create_event, name="create_event")
+    path("create_event/<int:pageId>/", create_event, name="create_event"),
+    path("create_event/<int:pageId>/<int:eventId>", update_event, name="update_event")
 ]
--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from events.forms import EventModelForm, ParticipantsModelForm, CategoryModelForm
 from django.shortcuts import redirect
 from django.contrib import messages
+from events.models import Event, Participant, Category
 
 # Create your views here.
 def home(request):
@@ -10,9 +11,9 @@ def home(request):
 def dashboard(request):
     return render(request, "dashboard.html")
 
-def create_event(request, id):
+def create_event(request, pageId):
     if(request.method == "POST"):
-        if id == 1:
+        if pageId == 1:  # 1 => shows create event form
             event_form = EventModelForm(request.POST)
             print("check 1")
             if event_form.is_valid():
@@ -20,28 +21,79 @@ def create_event(request, id):
                 print("check 2")
                 messages.success(request, "Event saved successfully.")
             
-        elif id == 2:
+        elif pageId == 2:  # 2 ==> shows create participant form
             event_form = ParticipantsModelForm(request.POST)
             if event_form.is_valid():
                 print(event_form.cleaned_data)
                 event_form.save()
                 messages.success(request, "Participant created successfully.")
 
-        elif id == 3:
+        elif pageId == 3:
             event_form = CategoryModelForm(request.POST)
             if event_form.is_valid():
                 event_form.save()
                 messages.success(request, "Category created successfully.")
 
-        return redirect("create_event", id)
+        return redirect("create_event", pageId)
         
     else :
-        event_form = EventModelForm(request.GET)
-        Participant_form = ParticipantsModelForm(request.GET)
-        cat_form = CategoryModelForm(request.GET)
+        event_form = EventModelForm()
+        Participant_form = ParticipantsModelForm()
+        cat_form = CategoryModelForm()
         context = {
-            "form": event_form if id == 1 else Participant_form if id == 2 else cat_form,
-            "id": id
+            "type": "create",
+            "form": event_form if pageId == 1 else Participant_form if pageId == 2 else cat_form,
+            "pageId": pageId
+        }
+        return render(request, "create_event.html", context)
+        
+def update_event(request, pageId, eventId):
+    event = None
+    if(request.method == "POST"):
+        if pageId == 1:  # 
+            event = Event.objects.get(id = eventId)
+            event_form = EventModelForm(request.POST, instance = event)
+            print("check 1")
+            if event_form.is_valid():
+                event_form.save()
+                print("check 2")
+                messages.success(request, "Event saved successfully.")
+            
+        elif pageId == 2:
+            event = Participant.objects.get(id = eventId)
+            event_form = ParticipantsModelForm(request.POST, instance = event)
+            if event_form.is_valid():
+                print(event_form.cleaned_data)
+                event_form.save()
+                messages.success(request, "Participant created successfully.")
+
+        elif pageId == 3:
+            event = Category.objects.get(id = eventId)
+            event_form = CategoryModelForm(request.POST, instance = event)
+            if event_form.is_valid():
+                event_form.save()
+                messages.success(request, "Category created successfully.")
+
+        return redirect("update_event", pageId = pageId, eventId = eventId)
+        
+    else :
+        if pageId == 1:  # 
+            event = Event.objects.get(id = eventId)
+            
+        elif pageId == 2:
+            event = Participant.objects.get(id = eventId)
+            
+        elif pageId == 3:
+            event = Category.objects.get(id = eventId)
+            
+        print(event)
+        event_form = EventModelForm(instance = event)
+        Participant_form = ParticipantsModelForm(instance = event)
+        cat_form = CategoryModelForm(instance = event)
+        context = {
+            "type": "update",
+            "form": event_form if pageId == 1 else Participant_form if pageId == 2 else cat_form,
+            "pageId": pageId
         }
         return render(request, "create_event.html", context)
         
