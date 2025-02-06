b6e989e9b3a0611ac6c1f812140bbf10fdc43ac8
tanvir <tanvirulislam149@gmail.com>
2025-02-06 16:25:49 +0600

    dashboard layout

--- event_management/urls.py
+++ event_management/urls.py
@@ -21,6 +21,6 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home),
+    path("", home, name="home"),
     path("events/", include("events.urls"))
 ] + debug_toolbar_urls()
