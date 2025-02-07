6e660bb7869ff06b837aced505c46badac358dee
tanvir <tanvirulislam149@gmail.com>
2025-02-07 22:04:07 +0600

    login done

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from users.forms import CustomRegisterForm
+from django.contrib.auth import authenticate, login
 
 
 
@@ -18,3 +19,17 @@ def register(request):
             return redirect("register")
     
     return render(request, "register.html", {"form": form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Wrong credentials. Please check again.")
+
+
+    return render(request, "login.html")
